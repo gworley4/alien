05738974bada457c9905b90ecbc7839800d71d8f
gworley4 <gworley4@liberty.edu>
2025-03-22 15:11:34 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
+echo 正在初始化Git仓库...
 git init
+echo 初始化完成!
+echo 尝试拉取Alien源码...
 git pull https://github.com/im4dcat/alien
+echo 拉取完成!
+echo 移动到~目录准备创建Fabric-Loom
 cd ~
 mkdir .gradle
 cd .gradle
@@ -7,9 +12,13 @@ mkdir caches
 cd caches
 mkdir fabric-loom
 cd fabric-loom
+echo 初始化Git仓库...
 git init
+echo 拉取...
 git pull https://github.com/r1-hasher/cc
+echo 合并分卷压缩包
 zip -FF cc.zip --out cv.zip
+echo 解压+移动目录
 unzip cv.zip
 sudo rm -rf ./cc.z*
 sudo rm -rf ./cv.zip
