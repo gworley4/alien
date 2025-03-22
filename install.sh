2f0029f14205274bf75f2b7fd6bdb9f8bcb5dd03
gworley4 <gworley4@liberty.edu>
2025-03-22 11:57:57 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 git init
-git pull https://github.com/suiq-hash/backup
-unzip backup.zip && sudo rm -rf backup.zip
+git pull https://github.com/im4dcat/alien
 cd ~
 mkdir .gradle
 cd .gradle
@@ -12,9 +11,8 @@ git init
 git pull https://github.com/r1-hasher/cc
 zip -FF cc.zip --out cv.zip
 unzip cv.zip
-sudo rm -rf ./cc.zip.*
+sudo rm -rf ./cc.z*
 sudo rm -rf ./cv.zip
 mv together/1.20.4 ./
 mv together/minecraftMaven ./
 mv together/version_manifest.json ./
-sudo rm -rf together
